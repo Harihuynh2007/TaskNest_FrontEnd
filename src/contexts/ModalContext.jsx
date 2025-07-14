9fc9677f8384dec57556fb97952dd6d5d9f93cad
Harihuynh2007 <hminhhai2000@gmail.com>
2025-07-14 20:56:05 +0700

    wrap bằng useCallback

--- src/contexts/ModalContext.jsx
+++ src/contexts/ModalContext.jsx
@@ -1,18 +1,18 @@
 
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useCallback } from 'react';
 
 export const ModalContext = createContext();
 
 export function ModalProvider({ children }) {
   const [modals, setModals] = useState({});
 
-  const openModal = (modalId, props = {}) => {
+  const openModal = useCallback((modalId, props = {}) => {
     setModals(prev => ({ ...prev, [modalId]: { open: true, props } }));
-  };
+  },[]);
 
-  const closeModal = modalId => {
+  const closeModal = useCallback(modalId => {
     setModals(prev => ({ ...prev, [modalId]: { open: false, props: {} } }));
-  };
+  }, []);
 
   return (
     <ModalContext.Provider value={{ modals, openModal, closeModal }}>
