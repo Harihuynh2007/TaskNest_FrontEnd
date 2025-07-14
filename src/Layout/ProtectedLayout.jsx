d5c977988787d5fa953c035334869d395627fa0c
Harihuynh2007 <hminhhai2000@gmail.com>
2025-07-14 19:26:11 +0700

    Merge App.css into index.css for better organization

--- src/Layout/ProtectedLayout.jsx
+++ src/Layout/ProtectedLayout.jsx
@@ -2,7 +2,7 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../contexts';
 import Header from '../components/Header';
-import SideBar from '../components/SiderBar';
+import SiderBar from '../components/SiderBar';
 
 export default function ProtectedLayout({ children }) {
   const { user } = useContext(AuthContext);
@@ -10,7 +10,7 @@ export default function ProtectedLayout({ children }) {
     <div className="app-layout">
       <Header user={user} />
       <div className="layout-body">
-        <SideBar />
+        <SiderBar />
         <main>{children}</main>
       </div>
     </div>
