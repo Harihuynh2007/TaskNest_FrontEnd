9ae83d3e62bf7fbc7ca7930e477e24488c586c92
Harihuynh2007 <hminhhai2000@gmail.com>
2025-08-05 16:54:09 +0700

    💄 Cập nhật layout header cho Board và các trang khác theo chuẩn hóa mới

--- src/Layouts/WithHeaderAndSidebarLayout.jsx
+++ src/Layouts/WithHeaderAndSidebarLayout.jsx
@@ -8,7 +8,7 @@ export default function WithHeaderAndSidebarLayout({ children }) {
     <>
       <Header />
       <div style={{ width:'100%', maxWidth: '1200px', margin: '0 auto', padding: '0 16px' }}>
-        <Row className="g-0" style={{ height: 'calc(100vh - 56px)' }}>
+        <Row className="g-0" style={{ height: 'calc(100vh - var(--header-height))' }}>
           <SiderBar />
           <Col xs={9} className="bg-light p-4 overflow-auto">
             {children}
