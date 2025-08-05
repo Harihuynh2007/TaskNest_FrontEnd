1cd19e7ffdae2a6e48e517ff010d58e7cd288bc7
Harihuynh2007 <hminhhai2000@gmail.com>
2025-08-05 11:48:39 +0700

    Tạo file index để gọi api dễ hơn

---
+++ src/api/index.js
@@ -0,0 +1,15 @@
+// src/api/index.js
+
+// Dòng này import tất cả các hàm đã export từ file apiClient.js
+// và export chúng ra lại. `default` export sẽ được export với tên `apiClient`.
+export { default as apiClient } from './apiClient'; 
+
+// Dùng `export *` để export tất cả các named export từ các file còn lại.
+// Đây là cách viết gọn và hiệu quả nhất.
+
+export * from './authApi';
+export * from './boardApi';
+export * from './cardApi';
+export * from './listApi';
+export * from './workspaceApi';
+export * from './mockApi'; // Nếu bạn vẫn còn dùng mock
